6f9b43bbd6f294d44ba5297e0104a95a273ee813
VitaliiZaborskyi <barlabumbia@gmail.com>
2026-09-25 23:58:34 +0300

    R10.3.1 splash: use one slower routed animation

--- app/static/pilot-r8-shell.js
+++ app/static/pilot-r8-shell.js
@@ -160,7 +160,7 @@
   try{
     if(sessionStorage.getItem('bizet_route_splash')==='1'){
       sessionStorage.removeItem('bizet_route_splash');
-      requestAnimationFrame(()=>play({duration:2600}).finally(()=>document.documentElement.classList.remove('r10-route-loading')));
+      requestAnimationFrame(()=>play({duration:3400}).finally(()=>document.documentElement.classList.remove('r10-route-loading')));
       setTimeout(()=>document.documentElement.classList.remove('r10-route-loading'),SPLASH_FAILSAFE_MS+500);
     }
   }catch(_){document.documentElement.classList.remove('r10-route-loading')}
